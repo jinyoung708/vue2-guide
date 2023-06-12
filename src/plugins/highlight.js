701ba38f7a488a2bfe1bcb02d8919b3b6aecfb0b
jinyang <89575067+jinyoung708@users.noreply.github.com>
2023-06-13 08:40:24 +0900

    2023-06-13 추가2

--- src/plugins/highlight.js
+++
@@ -1,8 +0,0 @@
-// highlight v11.8.0
-// vue-highlightjs v1.3.3
-
-import Vue from 'vue'
-import VueHighlightJS from 'vue-highlightjs'
-import 'highlight.js/styles/github-dark.css' // 테마 css 파일 연결
-
-Vue.use(VueHighlightJS)
